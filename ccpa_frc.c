42b02f49e9bf38b606d4d716b9cec131a6be8444
pedroLucas25 <pedrolucas111193@hotmail.com>
2018-09-09 01:11:27 -0400

    Primeira parte de envio de arquivo completa faltando apenas o recebimento por parte do cliente e salvamento do arquivo binario

--- ccpa_frc.c
+++ ccpa_frc.c
@@ -7,18 +7,24 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/param.h>
+#include <sys/cdefs.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include "crc32.c"
+#include "base64.c"
  
-#define PORT     6100
+#define porta     6100
+#define MAXLINE 2048
 
 int main(int argc, char *argv[]) {
-    int n_sock, n, tamanho;
-    char buffer[2048];
-    char *mensagem = argv[1];
+	FILE* arq;
+    unsigned int n_sock, n, tamanho, tamArq, crc, entInt;
+    char buffer[MAXLINE], tamstr[MAXLINE], crcstr[MAXLINE], msgEnv[MAXLINE], *base64out, Linha[MAXLINE];
+    char *mensagem = "ola mundo, meu nome eh pedro!";
     struct sockaddr_in end_serv;
 //                        Address Family   Datagram
-    if ( (n_sock = socket(AF_INET,         SOCK_DGRAM, 0)) < 0 ) {
+    if ((n_sock = socket(AF_INET,         SOCK_DGRAM, 0)) < 0 ) {
         perror("Falha na criacao do socket");
         exit(EXIT_FAILURE);
     }
@@ -27,17 +33,50 @@ int main(int argc, char *argv[]) {
     *	Escrever aqui o codigo do trabalho utilizando as funções de enviar e receber abaixo!!!
     */
     
+    if(strcmp(argv[1],"-e") == 0){
+    	arq = fopen(argv[2], "rb");
+
+  	    if (arq == NULL){  // Se houve erro na abertura
+     		perror("Problemas na abertura do arquivo\n");
+     		return EXIT_FAILURE;
+  		}
+  		
+  		fseek(arq, 0, SEEK_END);
+    	tamArq = ftell(arq);//Tamanho do arquivo %d
+    	fseek(arq, 0, SEEK_SET);
+    	
+    	fgets(Linha, MAXLINE, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
+      	Linha[strlen(Linha)-1] = '\0'; 	
+    	crc = xcrc32(Linha, strlen(Linha), 0);// crc32 em %d
+    	
+    	snprintf(tamstr, MAXLINE, "%d", tamArq);//converte tamArq de inteiro para string
+    	snprintf(crcstr, MAXLINE, "%x", crc);//converte crc de inteiro para string   
+    	//entInt = atoi(entBin);
+    	
+    	base64out = base64_encode(Linha);	
+    	
+    	strcat(msgEnv, "env,");
+    	strcat(msgEnv, tamstr);
+    	strcat(msgEnv, ",");
+    	strcat(msgEnv, "0x");
+    	strcat(msgEnv, crcstr);
+    	strcat(msgEnv, ",");
+    	strcat(msgEnv, argv[2]);
+    	strcat(msgEnv, ",");
+    	strcat(msgEnv, base64out);   	
+    }
+    
     end_serv.sin_family = AF_INET;
-    end_serv.sin_port = htons(PORT);
+    end_serv.sin_port = htons(porta);
     end_serv.sin_addr.s_addr = INADDR_ANY;
      
-    sendto(n_sock, (const char *)mensagem, strlen(mensagem), MSG_CONFIRM, (const struct sockaddr *) &end_serv, sizeof(end_serv));
-    //printf("mensagem message sent.\n");
+    sendto(n_sock, (const char *)msgEnv, strlen(msgEnv), MSG_CONFIRM, (const struct sockaddr *) &end_serv, sizeof(end_serv));
          
-    n = recvfrom(n_sock, (char *)buffer, 2048, MSG_WAITALL, (struct sockaddr *) &end_serv, &tamanho);
+    n = recvfrom(n_sock, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &end_serv, &tamanho);
     buffer[n] = '\0';
-    //printf("Server : %s\n", buffer);
+    printf("%s\n", buffer);
  
     close(n_sock);
+    fclose(arq);
     return 0;
 }
--- scpa_frc.c
+++ scpa_frc.c
@@ -8,49 +8,93 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/param.h>
+#include <sys/cdefs.h>
+#include "crc32.c"
+#include "base64.c"
  
 #define PORT     6100
-#define MAXLINE 1024
- 
-// Driver code
+#define MAXLINE 2048 
+
 int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from luana";
+	uint32_t decoMsg[MAXLINE], crc;
+    int sockfd, i, j, k, l, len, n, contVirg=0;
+    char buffer[MAXLINE], conteudoArquivo[MAXLINE], *decRecebido, crcRecebido[MAXLINE];
+    char crcstr[MAXLINE], crcHX[MAXLINE] = {"0x"}, nomeRecebido[MAXLINE], *nome64, msgEnv[MAXLINE] = {};
     struct sockaddr_in servaddr, cliaddr;
      
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Falha na criação do socket!");
         exit(EXIT_FAILURE);
     }
+    
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
      
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-     
-    // Filling server information
     servaddr.sin_family    = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
      
-    // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-            sizeof(servaddr)) < 0 )
-    {
-        perror("bind failed");
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+        perror("Falha de conexão!");
         exit(EXIT_FAILURE);
     }
      
-    int len, n;
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                &len);
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
     buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
-    printf("Hello message sent.\n"); 
+    //printf("%s\n", buffer);
+    
+    if(buffer[0] == 'e'){
+    	j=0;
+    	k=0;
+    	l=0;
+    	for(i=0;i<n;i++){
+    		if(buffer[i] == ','){
+    			contVirg++;
+    		}
+    		
+    		if(contVirg == 2){
+    			crcRecebido[j] = buffer[i+1];
+    			j++;
+    		}
+    		
+    		if(contVirg == 3){
+    			nomeRecebido[k] = buffer[i+1];
+    			k++;
+    		}
+    		
+    		if(contVirg >= 4){
+    			conteudoArquivo[l] = buffer[i+1];
+    			l++;
+    		}
+    	}
+    	
+    	crcRecebido[strlen(crcRecebido)-1] = '\0';
+    	nomeRecebido[strlen(nomeRecebido)-1] = '\0';
+    	
+    	nome64 = base64_encode(nomeRecebido);
+    	
+    	decRecebido = base64_decode(conteudoArquivo);
+    	
+    	//b64_decode(conteudoArquivo, strlen(conteudoArquivo), decoMsg);
+		crc = xcrc32(decRecebido, strlen(decRecebido), 0);
+    	snprintf(crcstr, MAXLINE, "%x", crc);
+    	
+    	strcat(crcHX, crcstr);
+    	
+    	if(strcmp(crcRecebido, crcHX) == 0){
+    		strcat(msgEnv, "res,0,");
+    		strcat(msgEnv, crcHX);
+    		strcat(msgEnv, ",");
+    		strcat(msgEnv, nome64);     		   		
+    	} else {
+    		strcat(msgEnv, "res,-1,0xFFFFFFFF");
+    		strcat(msgEnv, ",");
+    		strcat(msgEnv, nome64);   		
+    	}   	
+    }
+    printf("%s\n", msgEnv);
+    
+    sendto(sockfd, (const char *)msgEnv, strlen(msgEnv), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
      
     return 0;
 }
